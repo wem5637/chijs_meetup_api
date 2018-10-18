f11d2d6a2f9f887757af86adad88dde5428361e0
Will McCracken <wem5637@gmail.com>
2018-10-17 23:02:19 -0400

    Rehashed api for a rehashed chi js page

--- server/meetup.js
+++ server/meetup.js
@@ -1,45 +1,62 @@
 const axios = require('axios');
 
+//add to meetupOrgs to have them included in request
+const meetupOrgs = [
+  'Chicago-NodeJs',
+  'React-Chicago',
+  'js-chi'
+]
+
+let events = []
+let groups = []
+
+function getEventInfo(){
+  const mtpReqs = meetupOrgs.map((meetup)=>{
+    return axios.get(`https://api.meetup.com/${meetup}/events?&page=3`)
+  })
+  Promise.all(mtpReqs)
+  .then((evts)=>{
+    evts = evts.map((mtp)=>{
+      return mtp.data
+    });
+    evts = [].concat.apply([], evts).sort((a,b)=>{return a.time-b.time});
+
+    events = evts;
+  })
+  .catch(error => console.error('error /meetup/js'));
+}
+
+function getGroupInfo(){
+  const mtpReqs = meetupOrgs.map((meetup)=>{
+    return axios.get(`https://api.meetup.com/${meetup}`)
+  })
+  Promise.all(mtpReqs)
+  .then((grps)=>{
+    grps = grps.map((mtp)=>{
+      return mtp.data
+    });
+
+    groups = grps;
+  })
+  .catch(error => console.error('error /meetup/js'));
+}
+
+getGroupInfo()
+getEventInfo()
+var CronJob = require('cron').CronJob;
+
+new CronJob('0 0 * * * *', function() {
+  getGroupInfo()
+  getEventInfo()
+}, null, true, 'America/Los_Angeles');
+
 
 module.exports = require('express').Router()
 
-.get('/node', (req,res,next) => {
+	.get('/events', (req,res,next) => {
+	  res.json(events)
+	})
 
-  axios.get('https://api.meetup.com/Chicago-NodeJs/events?&sign=true&photo-host=public&page=3')
-  .then(res => res.data)
-  .then(ok => {
-    // console.log(ok);
-    res.json(ok);
-  })
-  .catch(error => console.error('error /meetup/node'));
-})
-.get('/js', (req,res,next) => {
-
-  axios.get('https://api.meetup.com/js-chi/events?&sign=true&photo-host=public&page=3')
-  .then(res => res.data)
-  .then(ok => {
-    // console.log(ok);
-    res.json(ok);
-  })
-  .catch(error => console.error('error /meetup/js'));
-})
-.get('/react', (req,res,next) => {
-
-  axios.get('https://api.meetup.com/React-Chicago/events?&sign=true&photo-host=public&page=3')
-  .then(res => res.data)
-  .then(ok => {
-    // console.log(ok);
-    res.json(ok);
-  })
-  .catch(error => console.error('error /meetup/react'));
-})
-.get('/ang', (req,res,next) => {
-
-  axios.get('https://api.meetup.com/AngularJS-Chicago/events?&sign=true&photo-host=public&page=3')
-  .then(res => res.data)
-  .then(ok => {
-    // console.log(ok);
-    res.json(ok);
-  })
-  .catch(error => console.error('error /meetup/ang'));
-})
+	.get('/groups', (req,res,next) => {
+	  res.json(groups)
+	})
--- server/start.js
+++ server/start.js
@@ -19,10 +19,7 @@ module.exports = app
   // Morgan
   .use(morgan('dev'))
 
-  // Static
-  .use(express.static(resolve(__dirname, '..', 'public')))
-
-  // let that Cross-Origin stuff through baby
+  // let that Cross-Origin stuff through
   .use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X_Requested-With, Content-Type, Accept");
@@ -32,25 +29,11 @@ module.exports = app
   // Serve our api
   .use('/api', require('./api'))
 
-  // any requests with an extension (.js, .css, etc.) turn into 404
-  .use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
-    } else {
-      next()
-    }
-  })
-
-  // Send index.html for anything else.
-  .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
-
   // Error middleware interceptor, delegates to same handler Express uses.
   .use((err, req, res, next) => {
     res.send(console.error(err))
   })
 
-  // start that shit up
+  // start server
   .listen(process.env.PORT||3001, console.log('listening on 3001!'))
 
