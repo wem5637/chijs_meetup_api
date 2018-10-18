4eb86412de522c9f490ccc9174364d4242e3fbb2
Will McCracken <wem5637@gmail.com>
2018-10-17 23:24:10 -0400

    prettifies

--- server/api.js
+++ server/api.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const api = module.exports = require('express').Router()
 
 api
-  .get('/heartbeat', (req, res) => res.send({ok: true}))
+  .get('/heartbeat', (req, res) => res.send({ ok: true }))
   .use('/meetup', require('./meetup'))
 
 api.use(morgan('dev'))
